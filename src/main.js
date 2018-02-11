3f04679da0579ab000bc37dca5f2516204640d9b
Vlad Hilko <vladislav.hilko@monostacklabs.com>
2018-02-11 12:54:18 +0300

    Some fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,54 @@
 (() => {
+
+  class Article {
+    constructor(articleContainer, jsonArticle) {
+      this.articleContainer = articleContainer;
+      this.article = document.createElement('div');
+      this.article.className = "article";
+      if(jsonArticle.urlToImage != null) this.articleContainer.appendChild(this.article);
+      this.jsonArticle = jsonArticle;
+    }
+
+    addArticle() {
+      this.addTitle();
+      this.addSourceName();
+      this.addDescription();
+      this.addImage();
+    }
+
+    addTitle() {
+      let title = document.createElement('a');
+      title.textContent = this.jsonArticle.title;
+      title.className = 'title';
+
+      this.article.appendChild(title);
+    }
+
+    addSourceName() {
+      let sourceName = document.createElement('h6');
+      sourceName.textContent = this.jsonArticle.source.name;
+      this.article.appendChild(sourceName);
+    }
+
+    addDescription() {
+      let description = document.createElement('p');
+      description.textContent = this.jsonArticle.description;
+      this.article.appendChild(description);
+    }
+
+    addImage() {
+      let imageLink = document.createElement('a');
+      imageLink.href = this.jsonArticle.url;
+
+      let urlToImage = document.createElement('img');
+      urlToImage.src = this.jsonArticle.urlToImage;
+
+      imageLink.appendChild(urlToImage);
+
+      this.article.appendChild(imageLink);
+    }
+  }
+
   const url = 'https://newsapi.org/v2/top-headlines?' +
             'country=ru&' +
             'apiKey=2a563e8347f14a44b618e0501345f3ff';
@@ -8,53 +58,59 @@
       const articleContainer = document.getElementById('article-container');
 
       return data.articles.map((jsonArticle) => {
-        let article = document.createElement('div');
-        article.className = "article";
-
-        if(jsonArticle.urlToImage != null) {
-          addSourceName(article, jsonArticle);
-          addTitle(article, jsonArticle);
-          addDescription(article, jsonArticle);
-          addImage(article, jsonArticle);
-          articleContainer.appendChild(article);
-        }
+        new Article(articleContainer, jsonArticle).addArticle();
       })
     })
-
-  let addTitle = (article, jsonArticle) => {
-    let title = document.createElement('a');
-    title.textContent = jsonArticle.title;
-    title.className = 'title';
-
-    article.appendChild(title);
-  }
-
-  let addSourceName = (article, jsonArticle) => {
-    let sourceName = document.createElement('h6');
-    sourceName.textContent = jsonArticle.source.name;
-    article.appendChild(sourceName);
-  }
-
-  let addDescription = (article, jsonArticle) => {
-    let description = document.createElement('p');
-    description.textContent = jsonArticle.description;
-    article.appendChild(description);
-  }
-
-  let addImage = (article, jsonArticle) => {
-    let imageLink = document.createElement('a');
-    imageLink.href = jsonArticle.url;
-
-    let urlToImage = document.createElement('img');
-    urlToImage.src = jsonArticle.urlToImage;
-
-    imageLink.appendChild(urlToImage);
-
-    article.appendChild(imageLink);
-  }
 })();
 
 (() => {
+
+  class ShortArticle {
+    constructor(popularAsideBlock, jsonArticle) {
+      this.popularAsideBlock = popularAsideBlock;
+
+      this.shortArticle = document.createElement('a');
+      this.shortArticle.className = "editor text-center";
+      this.shortArticle.href = jsonArticle.url;
+
+      if(jsonArticle.urlToImage != null) this.popularAsideBlock.appendChild(this.shortArticle);
+      this.jsonArticle = jsonArticle;
+    }
+
+    addShortArticle() {
+      this.addTitle();
+      this.addShortDescription();
+      this.addPublishedAt();
+      this.addUnderscore();
+    }
+
+    addTitle() {
+      let title = document.createElement('a');
+      title.textContent = this.jsonArticle.title;
+      title.className = 'title';
+
+      this.shortArticle.appendChild(title);
+    }
+
+    addShortDescription() {
+      let shortDescription = document.createElement('p');
+      shortDescription.textContent = this.jsonArticle.description.substring(0, 100) + '...';
+
+      this.shortArticle.appendChild(shortDescription);
+    }
+
+    addPublishedAt() {
+      let publishedAt = document.createElement('label');
+      publishedAt.textContent = this.jsonArticle.publishedAt;
+      this.shortArticle.appendChild(publishedAt);
+    }
+
+    addUnderscore() {
+      let underscore = document.createElement('span');
+      this.shortArticle.appendChild(underscore);
+    }
+  }
+
   const url = 'https://newsapi.org/v2/everything?' +
           'q=Russia&' +
           'language=ru&' +
@@ -68,41 +124,8 @@
       const popularAsideBlock = document.getElementById('popular-aside-block');
 
       return data.articles.map((jsonArticle) => {
-        let shortArticle = document.createElement('a');
-        shortArticle.className = "editor text-center";
-        shortArticle.href = jsonArticle.url;
-
-        addTitle(shortArticle, jsonArticle);
-        addShortDescription(shortArticle, jsonArticle);
-        addPublishedAt(shortArticle, jsonArticle);
-        addUnderscore(shortArticle, jsonArticle);
-
-        popularAsideBlock.appendChild(shortArticle);
+        new ShortArticle(popularAsideBlock, jsonArticle).addShortArticle();
       })
     })
 
-  let addTitle = (shortArticle, jsonArticle) => {
-    let title = document.createElement('h3');
-    title.textContent = jsonArticle.title;
-
-    shortArticle.appendChild(title);
-  }
-
-  let addShortDescription  = (shortArticle, jsonArticle) => {
-    let shortDescription = document.createElement('p');
-    shortDescription.textContent = jsonArticle.description.substring(0, 100) + '...';
-
-    shortArticle.appendChild(shortDescription);
-  }
-
-  let addPublishedAt = (shortArticle, jsonArticle) => {
-    let publishedAt = document.createElement('label');
-    publishedAt.textContent = jsonArticle.publishedAt;
-    shortArticle.appendChild(publishedAt);
-  }
-
-  let addUnderscore = (shortArticle, jsonArticle) => {
-    let underscore = document.createElement('span');
-    shortArticle.appendChild(underscore);
-  }
 })();
